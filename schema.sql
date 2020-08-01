c3268f6e322b3d0f44b3e5d268ad63baf595aca9
Curtis Malainey <curtis@malainey.com>
2020-08-01 12:41:45 -0700

    remove table sql since we are using models now

--- schema.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS configs;
-CREATE TABLE  configs ( email VARCHAR(120) PRIMARY KEY,  VARCHAR(20), token char(200), secret CHAR(200));
-DROP TABLE IF EXISTS video_cache;
-CREATE TABLE youtube_cache (id char(11) PRIMARY KEY, title VARCHAR(120), thumbnail_url VARCHAR(400));

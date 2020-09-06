0cc36c8acbdccbe228b9042556cbc70b4fc6d289
Curtis Malainey <curtis@malainey.com>
2020-09-06 12:41:48 -0700

    make things pretty

--- music_queue/static/queue_app.js
+++ music_queue/static/queue_app.js
@@ -16,10 +16,21 @@ function trim_queue(data) {
 }
 
 function build_elements(data) {
-  t = document.createElement("li");
-  $(t).html(data['title']);
-  $(t).hide();
-  return t;
+  li = document.createElement("li");
+  d = document.createElement("div");
+  h1 = document.createElement("h1");
+  h3 = document.createElement("h3");
+  img = document.createElement("img");
+  $(li).append(d);
+  $(d).append(img);
+  $(d).append(h1);
+  $(d).append(h3);
+  $(img).attr("src", data['thumbnails']['default']['url']);
+  $(img).addClass("thumbnail");
+  $(h1).html(data['title']);
+  $(h3).html(data['channelTitle']);
+  $(li).hide();
+  return li;
 }
 
 function remove_element(node) {
@@ -34,7 +45,7 @@ function insert_queue(value, index, array) {
   var remove_count = 0;
   for (i = index; i + remove_offset < l.length; i++) {
     var offset = remove_offset + i;
-    if ($(l[offset]).html() == value['title']) {
+    if ($(l[offset]).find('h1').html() == value['title']) {
       found = true;
       break;
     } else {
@@ -46,6 +57,11 @@ function insert_queue(value, index, array) {
     new_node = build_elements(value);
     $("#queue").append(new_node);
     $(new_node).slideDown();
+  }
+  if (index == 0) {
+    $(l[offset]).find("div").addClass("rainbow");
+  } else {
+    $(l[offset]).find("div").removeClass("rainbow");
   }
   remove_offset += remove_count;
 }

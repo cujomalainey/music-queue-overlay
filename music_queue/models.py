0e89d1aaba0edba2de1f35727185e55044d89c49
Curtis Malainey <curtis@malainey.com>
2020-10-22 21:24:16 -0700

    add cache

--- music_queue/models.py
+++ music_queue/models.py
@@ -7,3 +7,10 @@ class YTVideo(db.Model):
 
     def __repr__(self):
         return '<Video: {}>'.format(self.title)
+
+class ResultCache(db.Model):
+    id = db.Column(db.String(240), primary_key=True)
+    pickle = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<cache: {}>'.format(self.id)
